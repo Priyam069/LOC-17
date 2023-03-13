c089dd7792613bca15d0d34287b01841b2844e8c
Priyam069 <127723017+Priyam069@users.noreply.github.com>
2023-03-13 21:20:20 +0530

    Create Q4.c

---
+++ week 1/Q4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, choice;
+
+    printf("Enter first number:\n");
+    scanf("%d", &a);
+    printf("Enter second number:\n");
+    scanf("%d", &b);
+    
+    printf("\nList of operations \n1 for Addition \n2 for Substraction \n3 for Multiplication \n4 for Division");
+
+    printf("\nEnter your choice: ");
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            printf("Addition of %d + %d = %d\n", a, b, a + b);
+            break;
+
+        case 2:
+            printf("Substraction of %d - %d = %d\n", a, b, a - b);
+            break;
+
+        case 3:
+            printf("Multiplication of %d * %d = %d\n", a, b, a * b);
+            break;
+
+        case 4:
+            printf("Division of %d / %d = %f\n", a, b, (float)a / b);
+            break;
+        default:
+            printf("\nWrong choice\n");
+            break;
+    }
+    return 0;
+}
