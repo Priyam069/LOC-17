265eb7f96d81cffa4b1ecc34ed8d761302c8d686
Priyam Behera <127723017+Priyam069@users.noreply.github.com>
2023-03-13 22:49:40 +0530

    Update Q1.c

--- week 1/Q1.c
+++ week 1/Q1.c
@@ -4,11 +4,15 @@ int main()
     int i;
     for (i=1; i<=100; i++)
     {
-        if ((i%3) == 0) 
+        if ((i%3) == 0 && i%5==0)
         {
-            printf("Fizz\t"); 
+            printf("FizzBuzz\t"); 
         }
-        else if ((i%5) == 0) 
+        else if (i%3==0)
+        {
+            printf("Fizz\t");
+        }
+        else if (i%5==0) 
         {                     
             printf("Buzz\t");   
         }
